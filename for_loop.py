0c49f611ce02afea8616f48e07df943c398d636e
SamFajobi <olusegunfajobi12@gmail.com>
2025-01-29 13:03:07 +0100

    Inbuilt functions

--- for_loop.py
+++ for_loop.py
@@ -1,4 +1,7 @@
-str1 = "Hello World"
-str2 = "How is it going??"
-result = str1 + "" + str2
-print(result)
+##############################################
+
+# str1 = "Hello World"
+# str2 = "How is it going??"
+# result = str1 + "" + str2
+# print(result)
+
---
+++ inbuiltfunction.py
@@ -0,0 +1,22 @@
+
+# Inbuilt Functions
+
+# split()
+arn = "arn:aws:iam::12345778:user/johndoe"
+print (arn.split("/")[1]);
+
+name = "Fajobi Olusegun Samuel"
+print("Name is:", name.split())
+
+text = "Python is awesome"
+words = text.split()
+print("Words:", words)
+
+
+# replace()
+
+
+# strip()
+
+
+#
