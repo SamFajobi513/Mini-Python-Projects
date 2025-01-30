6a3331f9509b5be4cba08887d74876f725e9628f
SamFajobi <olusegunfajobi12@gmail.com>
2025-01-30 22:51:45 +0100

    functions

--- functions.py
+++ functions.py
@@ -9,3 +9,32 @@ def add(num1, num2):
 
 add(7,3)    
 
+
+
+# A function can take input values (parameters).
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")
+greet("Bob")
+
+
+
+# A function can return a value using the return statement.
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print(result)  # Output: 8
+
+
+
+# Default Parameter Values(If a parameter has a default value, it becomes optional when calling the function)
+def greet(name="Guest"):
+    print(f"Hello, {name}!")
+
+greet()        # Output: Hello, Guest!
+greet("Alice") # Output: Hello, Alice!
+
+
+# 
