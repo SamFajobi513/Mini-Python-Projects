2b397a09c7bd588800261c2d421c82195a321591
SamFajobi <olusegunfajobi12@gmail.com>
2025-01-31 20:35:49 +0100

    operators

---
+++ operators.py
@@ -0,0 +1,7 @@
+# Arithmetic Operations in Python
+
+## Introduction
+
+Arithmetic operators in Python allow you to perform basic mathematical calculations on numeric values. These operators include addition, subtraction, multiplication, division, and more.
+
+## List of Arithmetic Operators
