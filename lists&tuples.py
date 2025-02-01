c3dd26d3f0e56db1fb56bcaa6d40947557544112
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-01 21:58:35 +0100

    list

--- lists&tuples.py
+++ lists&tuples.py
@@ -1,12 +1,18 @@
 # Creating a list
 my_list = [10, "hello", 3.14, True]
 
-# Accessing elements
+# Accessing elements(List Indexing)
 print(my_list[1])  # Output: hello
 
 # Modifying elements
 my_list[0] = 99
 print(my_list)  # Output: [99, 'hello', 3.14, True]
+
+#Print Lists Length
+print(len(my_list)) # Output: 4
+
+#Slicing a Lists
+print(my_list[1:4]) #Creates a new list with elements at index 1,2,3
 
 # Adding elements
 my_list.append("Python")
@@ -16,6 +22,10 @@ print(my_list)  # Output: [99, 'hello', 3.14, True, 'Python']
 my_list.remove(3.14)
 print(my_list)  # Output: [99, 'hello', True, 'Python']
 
-# Iterating through a list
+# Iterating through a list and printing all elements in a List.
 for item in my_list:
     print(item)
+
+#Checking if an element is present in a List
+is_present = "boy" in my_list
+print(is_present)
