a4f92ff4fd9899772c9cae6bb447a1306ba029cd
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-01 21:37:33 +0100

    list_tuples

---
+++ lists&tuples.py
@@ -0,0 +1,11 @@
+# Creating a list
+my_list = [10, "hello", 3.14, True]
+
+# Accessing elements
+print(my_list[1])  # Output: hello
+
+# Modifying elements
+my_list[0] = 99
+print(my_list)  # Output: [99, 'hello', 3.14, True]
+
+
