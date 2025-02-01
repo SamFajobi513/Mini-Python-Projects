d0899b32a9b862e5209dd96ba19ce3c68147f226
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-01 21:41:36 +0100

    list

--- lists&tuples.py
+++ lists&tuples.py
@@ -8,4 +8,14 @@ print(my_list[1])  # Output: hello
 my_list[0] = 99
 print(my_list)  # Output: [99, 'hello', 3.14, True]
 
+# Adding elements
+my_list.append("Python")
+print(my_list)  # Output: [99, 'hello', 3.14, True, 'Python']
 
+# Removing elements
+my_list.remove(3.14)
+print(my_list)  # Output: [99, 'hello', True, 'Python']
+
+# Iterating through a list
+for item in my_list:
+    print(item)
