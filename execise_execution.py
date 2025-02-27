8709d1a6eeef29d5c6a2b126554168f3a89c57e4
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-27 23:57:48 +0100

    python-exercise

---
+++ execise_execution.py
@@ -0,0 +1,11 @@
+# for i in range(1, 10):
+#     print("current value is:", i)
+
+# name = eval(input("enter your name:"))
+# print("your name is:", name)
+
+# for i in range(4):
+#     num = eval(input("Enter a number:"))
+#     print("The square of the number is:", num*num)
+# print("The Loop is finished")
+
